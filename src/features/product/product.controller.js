d74c82e55039f35d3d8a7b236c2f30cee993e586
Anant Kumar <ak8002749@gmail.com>
2024-02-05 20:49:10 +0530

    Added Add product API

--- src/features/product/product.controller.js
+++ src/features/product/product.controller.js
@@ -6,9 +6,16 @@ export default class ProductController{
         res.status(200).send(products);
     }
     addProduct(req,res){
-        console.log(req.body);
-        console.log("This is a post request");
-        res.status(200).send("Post Request Received");
+        // console.log(req.body);
+        const {name,price,sizes}=req.body;
+        const newProduct={
+            name,
+            price:parseFloat(price),
+            sizes:sizes.split(','),
+            imageUrl:req.file.filename,
+        }
+        const createdRecord=ProductModel.add(newProduct);
+        res.status(201).send(createdRecord);
     }
     rateProduct(req,res){
 
--- src/features/product/product.routes.js
+++ src/features/product/product.routes.js
@@ -3,6 +3,7 @@
 //1. Import express
 import express from "express";
 import ProductController from "./product.controller.js";
+import { upload } from "../../middlewares/fileUpload.middleware.js";
 
 //2. Initialize Express router
 const ProductRouter=express.Router();
@@ -10,6 +11,7 @@ const ProductRouter=express.Router();
 const productController = new ProductController();
 //All the paths to controller methods.
 ProductRouter.get("/", productController.getAllProduct);
-ProductRouter.post("/",productController.addProduct);
+ProductRouter.post("/",upload.single('imageUrl'),productController.addProduct);
 
 export default ProductRouter;
+  
