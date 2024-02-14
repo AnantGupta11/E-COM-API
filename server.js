0a24fb7931fe6b9c965a1ea979f33cc79ec1c2d6
Anant Kumar <ak8002749@gmail.com>
2024-02-14 17:32:06 +0530

    Added Open API and swagger

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // 1. Import Exprerss
 import express from 'express';
+import swagger from 'swagger-ui-express';
 import productRouter from './src/features/product/product.routes.js';
 import userRouter from './src/features/user/user.routes.js';
 import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
 import jwtAuth from './src/middlewares/jwt.middleware.js';
 import cartRouter from './src/features/cartItems/cartItem.routes.js';
+import apiDocs from './swagger.json' assert {type:'json'};
+
 // 2. Create Server
 const server = express();
 
@@ -12,6 +15,7 @@ server.use(express.json());
 
 // for all requests related to product, redirect to product routes.
 // localhost:3200/api/productss
+server.use("/api-docs",swagger.serve, swagger.setup(apiDocs));
 server.use('/api/products', jwtAuth, productRouter);
 server.use('/api/users', userRouter);
 server.use('/api/cartItems',jwtAuth, cartRouter);
