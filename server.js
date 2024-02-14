d3064caf229b2e6699979ca02ded52abe99cde7c
Anant Kumar <ak8002749@gmail.com>
2024-02-14 21:17:51 +0530

    Added CORS Policy

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ import apiDocs from './swagger.json' assert {type:'json'};
 // 2. Create Server
 const server = express();
 
+// CORS policy configuration
+server.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","http://localhost:5500");
+  res.header("Access-Control-Allow-Headers",'*');
+  res.header("Access-Control-Allow-Methods",'*');
+  //return ok for preflight request
+  if(req.method=="OPTIONS"){
+    return res.sendStatus(200);
+  }
+})
+
 server.use(express.json());
 
 // for all requests related to product, redirect to product routes.
