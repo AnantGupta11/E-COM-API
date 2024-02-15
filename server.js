b518722b6221fd5fc59554892c034ec96d70cb96
Anant Kumar <ak8002749@gmail.com>
2024-02-15 19:44:15 +0530

    Added logger for logging all types of request

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
 // 1. Import Exprerss
 import express from 'express';
 import swagger from 'swagger-ui-express';
+import cors from 'cors';
 import productRouter from './src/features/product/product.routes.js';
 import userRouter from './src/features/user/user.routes.js';
 import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
 import jwtAuth from './src/middlewares/jwt.middleware.js';
 import cartRouter from './src/features/cartItems/cartItem.routes.js';
 import apiDocs from './swagger.json' assert {type:'json'};
+import loggerMiddleware from './src/middlewares/logger.middleware.js';
 
 // 2. Create Server
 const server = express();
 
 // CORS policy configuration
-server.use((req,res,next)=>{
-  res.header("Access-Control-Allow-Origin","http://localhost:5500");
-  res.header("Access-Control-Allow-Headers",'*');
-  res.header("Access-Control-Allow-Methods",'*');
-  //return ok for preflight request
-  if(req.method=="OPTIONS"){
-    return res.sendStatus(200);
-  }
-})
+server.use(cors());
+// server.use((req,res,next)=>{
+//   res.header("Access-Control-Allow-Origin","http://localhost:5500");
+//   res.header("Access-Control-Allow-Headers",'*');
+//   res.header("Access-Control-Allow-Methods",'*');
+//   //return ok for preflight request
+//   if(req.method=="OPTIONS"){
+//     return res.sendStatus(200);
+//   }
+// })
 
 server.use(express.json());
 
 // for all requests related to product, redirect to product routes.
 // localhost:3200/api/productss
 server.use("/api-docs",swagger.serve, swagger.setup(apiDocs));
+server.use(loggerMiddleware);
 server.use('/api/products', jwtAuth, productRouter);
 server.use('/api/users', userRouter);
 server.use('/api/cartItems',jwtAuth, cartRouter);
@@ -36,9 +40,10 @@ server.get('/', (req, res) => {
 });
 
 //4. middleware to handle 404 request
-server.use(req,res){
+server.use((req,res,next)=>{
   res.status(404).send("API not found");
-}
+  next();
+})
 
 // 5. Specify port.
 server.listen(3200);
---
+++ src/middlewares/logger.middleware.js
@@ -0,0 +1,25 @@
+import exp from 'constants';
+import fs from 'fs';
+
+const fsPromise=fs.promises;
+
+async function log(logData){
+    try{
+        logData=`\n ${new Date().toString()} - ${logData}`;
+        await fsPromise.appendFile("log.txt",logData);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const loggerMiddleware= async (req,res,next)=>{
+    //1.log req body
+    if(req.url.includes("signin")){
+        next();
+    }
+    const logData=`${req.url} - ${JSON.stringify(req.body)}`;
+    await log(logData);
+    next();
+}
+
+export default loggerMiddleware;
